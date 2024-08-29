307d4627b68fdf1af6b602ae91668fdc2b36240a
ynurr <0305_08@naver.com>
2024-08-29 21:45:38 +0900

    로그인한 유저만 작성 허용

--- app/write/page.js
+++ app/write/page.js
@@ -1,4 +1,18 @@
-export default function Write() {
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
+import { getServerSession } from "next-auth"
+
+export default async function Write() {
+
+    let session = await getServerSession(authOptions)
+
+    if (!session) {
+        return (
+            <div className="p-20">
+                <p>로그인 후 이용 가능합니다.</p>
+            </div>
+        )
+    }
+
     return (
         <div className="p-20">
             <h4>글작성</h4>
