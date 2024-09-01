64748339e3ce9c032d612ae3437f1e5b7e97e42e
ynurr <0305_08@naver.com>
2024-09-01 19:27:21 +0900

    댓글 조회 기능

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,14 +1,33 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Comment(props) {
 
     let [comment, setComment] = useState('')
+    let [data, setData] = useState([])
+
+    useEffect(()=>{
+        fetch('/api/comment/list?id=' + props._id)
+        .then(r=>r.json())
+        .then((result)=>{
+            setData(result)
+        })
+    },[])
+
     
     return (
         <div>
-            <div>댓글목록</div>
+            <div>댓글</div>
+            {
+                data.length > 0 ?
+                data.map((a,i)=>{
+                    return (
+                        <p key={i}>{a.content}</p>
+                    )
+                })
+                : '댓글 없음'
+            }
             <input onChange={(e)=>{ setComment(e.target.value) }}/>
             <button onClick={()=>{
                 fetch('/api/comment/new', {
---
+++ pages/api/comment/list.js
@@ -0,0 +1,8 @@
+import { connectDB } from "@/util/database"
+import { ObjectId } from "mongodb"
+
+export default async function handler(요청, 응답) {
+    const db = (await connectDB).db('forum')
+    let result = await db.collection('comment').find({ parent : new ObjectId(요청.query.id) }).toArray()
+    응답.status(200).json(result)
+}
